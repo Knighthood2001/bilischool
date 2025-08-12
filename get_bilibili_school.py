4df7133d4adb609d8bedb0233c7c55795c34f1e8
Knighthood2001 <2109695291@qq.com>
2025-08-12 13:06:41 +0800

    Update at 2025-08-12 13:06:40

--- get_bilibili_school.py
+++ get_bilibili_school.py
@@ -8,9 +8,6 @@ def get_up_school_info(uid):
         "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36",
         'Referer': "https://www.bilibili.com",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-   #         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        # 'Sec-Ch-Ua':'"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-        # "Sec-Ch-Ua-Platform":'"Android"',
     }
     
     try:
